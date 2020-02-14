d4b00726cc0d531a38aa1078ff21a22cbda3cf2c
Danny <goody49172@gmail.com>
2020-02-13 19:28:17 -0700

    Pictures Hate me!!!

--- Java/store.js
+++ Java/store.js
@@ -1,8 +1,10 @@
-var answer = document.getElementById("bar").innerHTML;
-var plush = [["plush1","plush2","plush3","plush4"]];
+
+var plush = new Array();
+plush[0] = new Image();
+plush[0].src ="images/plush.jpg";
 var figure = [["figure1","figure2","figure3","figure4"]];
 var hood = [["hood1","hood2","hood3","hood4"]];
-
+//var answer = document.getElementById("search").innerHTML;
 /*for(var x=0;x<3;x++)
 {
     for(var y=0;y<4; y++)
@@ -13,8 +15,21 @@ var hood = [["hood1","hood2","hood3","hood4"]];
 }*/
 function bar()
 {
+    var answer = document.getElementById("search").innerHTML;
 if (answer == "plush")
 {
-    document.getElementById("result").innerHTML = (plush);
+    console.log = (plush);
+}
+
+}
+
+function plushies()
+{
+for(var x=0;x<4;x++)
+{
+ 
+        console.log(plush[x]);
+        document.write("result <img src'" + plush[x]);
+    
 }
 }
